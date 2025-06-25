510a9b39beb75747ac523b68c09a812c3b7fe873
Erica <erica@gmail.com>
2025-06-25 20:21:42 -0300

    feat: export saveFearGreed in fearGreedService

--- src/services/fearGreedService.js
+++ src/services/fearGreedService.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
-import https from 'https';
-const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+﻿import { pool } from '../database.js';
 
-export async function fetchFearGreed() {
-  try {
-    const { data } = await axios.get(
-      'https://api.alternative.me/fng/?limit=1',
-      { httpsAgent, timeout: 5000 }
-    );
-    return data.data[0];
-  } catch (err) {
-    console.error('Erro ao buscar Fear & Greed:', err.message);
-    throw err;
-  }
+export async function saveFearGreed({ fear_greed: value, time }) {
+  await pool.query(
+    `INSERT INTO fear_greed (value, timestamp, captured_at)
+     VALUES ($1, $2, NOW())`,
+    [value, time]
+  );
+}
+
+/** Persiste no banco um novo valor de Fear & Greed. */
+export async function saveFearGreed({ fear_greed: value, time }) {
+  await pool.query(
+    \INSERT INTO fear_greed (value, \"timestamp\", captured_at) VALUES (, , NOW())\,
+    [value, time]
+  );
 }
