ae5a5bd1a7d688177e34860e4934e115750a4231
Erica <erica@gmail.com>
2025-06-25 21:12:13 -0300

    fix: remove duplicated saveFearGreed export

--- src/services/fearGreedService.js
+++ src/services/fearGreedService.js
@@ -1,31 +1,8 @@
-﻿import { pool } from '../database.js';
-
-export async function saveFearGreed({ fear_greed: value, time }) {
-  await pool.query(
-    `INSERT INTO fear_greed (value, timestamp, captured_at)
-     VALUES ($1, $2, NOW())`,
-    [value, time]
-  );
-}
-
+import { query } from './databaseService.js';
 /** Persiste no banco um novo valor de Fear & Greed. */
 export async function saveFearGreed({ fear_greed: value, time }) {
-  await pool.query(
-    \INSERT INTO fear_greed (value, \"timestamp\", captured_at) VALUES (, , NOW())\,
-    [value, time]
-  );
-}
-/** Persiste no banco um novo valor de Fear & Greed. */
-export async function saveFearGreed({ fear_greed: value, time }) {
-  await pool.query(
-    \INSERT INTO fear_greed (value, "timestamp", captured_at) VALUES (, , NOW())\,
-    [value, time]
-  );
-}
-/** Persiste no banco um novo valor de Fear & Greed. */
-export async function saveFearGreed({ fear_greed: value, time }) {
-  await pool.query(
-    \INSERT INTO fear_greed (value, \"timestamp\", captured_at) VALUES (, , NOW())\,
+  await query(
+    'INSERT INTO fear_greed (value, "timestamp", captured_at) VALUES ($1, $2, NOW())',
     [value, time]
   );
 }
