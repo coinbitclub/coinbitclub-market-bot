a85f29cb9bc95f0aa188067ae8e7e3ef4030c35f
Erica <erica@gmail.com>
2025-06-27 17:20:59 -0300

    fix: força SSL sem verificação de certificado para Railway

--- src/database.js
+++ src/database.js
@@ -1,14 +1,11 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-// Garante uso de SSL se URL pedir (e força disable do "verify" para Railway)
-const shouldUseSSL =
-  process.env.DATABASE_URL &&
-  (process.env.DATABASE_URL.includes('sslmode=require') || process.env.DATABASE_URL.includes('railway'));
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: shouldUseSSL ? { rejectUnauthorized: false } : false,  // false, não undefined!
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('error', err => {
