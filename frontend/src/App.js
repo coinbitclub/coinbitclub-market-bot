fa1ffbf56028e5bd281d0bd0dc310f360653b604
Erica <erica@gmail.com>
2025-07-01 16:40:39 -0300

    fix: atualização completa do frontend funcionando e sincronização total

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>CoinbitClub - Plataforma de Trading Automatizado</h1>
+      <p>Bem-vindo ao frontend!</p>
+    </div>
+  );
+}
+
+export default App;
