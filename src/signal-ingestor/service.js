f49efe58564fa8f8fa03ac9a9183c4d5651f3029
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-14 09:38:46 -0300

    Delete src directory

--- src/signal-ingestor/service.js
+++
@@ -1,4 +0,0 @@
-import { getDB } from '../common/db.js';
-export async function saveRawWebhook(route, payload) {
-  await getDB()('raw_webhook').insert({ route, payload, received_at: getDB().fn.now() });
-}
--- src/user-credentials/service.js
+++
@@ -1,7 +0,0 @@
-import { getDB } from '../common/db.js';
-export async function addCredentials(userId, cred) {
-  await getDB()('user_credentials').insert({ user_id: userId, ...cred });
-}
-export async function listCredentials(userId) {
-  return getDB()('user_credentials').where('user_id', userId);
-}
