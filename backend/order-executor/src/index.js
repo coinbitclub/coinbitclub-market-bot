3fc5cfc3e1e101fe88d41a08497aeeb0fcedbc41
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-14 16:15:05 -0300

    Update index.js

--- backend/order-executor/src/index.js
+++ backend/order-executor/src/index.js
@@ -3,22 +3,18 @@ import logger from '../../common/logger.js';
 import '../../common/env.js';
 
 import { ensureConnection } from '../../common/db.js';
-import { getChannel } from './rabbitmq.js';
+import { consume } from './rabbitmq.js';
 import express from 'express';
 import { initMetrics } from '../../common/metrics.js';
 
 async function start() {
   await ensureConnection();
-  const channel = await getChannel();
-  await channel.assertQueue('order.request');
-  channel.consume('order.request', async (msg) => {
+  await consume('order.request', async (order) => {
     try {
-      const order = JSON.parse(msg.content.toString());
       await executeOrder(order);
-      channel.ack(msg);
     } catch (err) {
       logger.error({ err }, 'order processing failed');
-      channel.nack(msg, false, false);
+      throw err;
     }
   });
 }
