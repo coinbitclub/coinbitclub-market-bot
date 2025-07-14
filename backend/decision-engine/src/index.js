dbe01e476480783532f8c4b0cec96d4a5a74634e
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-14 09:25:11 -0300

    Create index.js

---
+++ backend/decision-engine/src/index.js
@@ -0,0 +1,21 @@
+import amqp from 'amqplib';
+import { evaluate } from './rulesEngine.js';
+import { aiFallback } from './aiFallback.js';
+
+async function start() {
+  const conn = await amqp.connect(process.env.AMQP_URL || 'amqp://localhost');
+  const channel = await conn.createChannel();
+  await channel.assertQueue('signal.filtered');
+  await channel.assertQueue('order.request');
+  channel.consume('signal.filtered', async msg => {
+    const signal = JSON.parse(msg.content.toString());
+    let decision = evaluate(signal);
+    if (!decision) {
+      decision = await aiFallback(signal);
+    }
+    channel.sendToQueue('order.request', Buffer.from(JSON.stringify(decision)));
+    channel.ack(msg);
+  });
+}
+
+start();
