ffbe70aa181658ce9c927d92aceb43f27a6b8ead
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-14 22:19:54 -0300

    Update _app.tsx

--- coinbitclub-frontend-premium/src/pages/_app.tsx
+++ coinbitclub-frontend-premium/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+import ThemeProvider from '../components/ThemeProvider'
+import Footer from '../components/Footer'
+import { useEffect } from 'react'
+import { initSentry } from '../lib/sentry'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    initSentry()
+  }, [])
+
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  )
 }
