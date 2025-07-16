388508bdf6607cccabbcae655bfbdb473e794645
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-16 00:03:08 -0300

    Update knexfile.js

--- backend/api-gateway/knexfile.js
+++ backend/api-gateway/knexfile.js
@@ -1,10 +1,12 @@
-export default {
+const base = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
-  migrations: {
-    directory: './migrations'
-  },
-  seeds: {
-    directory: './seeds'
-  }
+  migrations: { directory: './migrations' },
+  seeds: { directory: './seeds' },
+};
+
+export default {
+  development: base,
+  test: base,
+  production: base,
 };
