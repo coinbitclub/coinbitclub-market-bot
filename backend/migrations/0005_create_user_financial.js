f0c51c7982c26185a6d04c39f4ab3750386cd471
coinbitclub <faleconosco@coinbitclub.vip>
2025-07-16 00:03:53 -0300

    Update 0005_create_user_financial.js

--- backend/migrations/0005_create_user_financial.js
+++ backend/migrations/0005_create_user_financial.js
@@ -2,7 +2,9 @@ export async function up(knex) {
   await knex.schema.createTable('user_financial', table => {
     table.increments('id').primary();
     table.uuid('user_id');
-    table.float('profit_loss');
+    table.float('balance').defaultTo(0);
+    table.float('profit_loss').defaultTo(0);
+    table.boolean('closed').defaultTo(false);
     table.timestamp('timestamp').defaultTo(knex.fn.now());
   });
 }
